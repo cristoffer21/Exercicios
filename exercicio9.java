9eb2558b76a2261b7736e2b305a7e866dac1af11
Cristoffer F Martins <107115156+cristoffer21@users.noreply.github.com>
2022-07-13 09:35:17 -0300

    Update exercicio9.java

--- exercicio9.java
+++ exercicio9.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
-
-public class exer4 {
-
-    public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
-        int n1, n2, n3, media;
-        String aluno;
-        System.out.println("Digite o nome do Aluno: ");
-        aluno = ler.nextLine();
-        System.out.println("Digite a primeira nota ");
-        n1 = ler.nextInt();
-        System.out.println("Digite a segunda nota ");
-        n2 = ler.nextInt();
-        System.out.println("Digite a terceira nota ");
-        n3 = ler.nextInt();
-        media = (n1 + n2 + n3) / 3;
-        System.out.println("O aluno " + aluno + " tem media de: " + media);
-
-    }
+import javax.swing.JOptionPane;
+public class exercicio9 {
+        public static void main(String[] args) {
+            float n1,n2,media;
+            Scanner var = new Scanner(System.in);
+            String aluno;
+            System.out.println("Digite a primeira nota ");
+            n1 = var.nextFloat();
+            System.out.println("Digite a segunda nota ");
+            n2 = var.nextFloat();
+            media = (n1 + n2) / 2;
+            JOptionPane.showMessageDialog(null, "resultado:"+media);
+        }
 }
